7346328baf313784628894da5553383399865eff
polyhistor2050 <chrisspaul2050@gmail.com>
2025-05-06 15:24:55 +0300

    Add update and delete methods

--- src/main/java/com/runnerz/run/RunController.java
+++ src/main/java/com/runnerz/run/RunController.java
@@ -5,9 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -27,11 +32,12 @@ public class RunController {
         return "Hello Runnerz";
     }
 
-    @GetMapping("/all")
+    @GetMapping("")
     List<Run> findAll() {
         return runRepository.findAll();
     }
 
+    // GET
     @GetMapping("/{id}")
     Run findById(@PathVariable Integer id) {
         Optional<Run> run = runRepository.findById(id);
@@ -40,6 +46,26 @@ public class RunController {
         }
 
         return run.get();
+    }
+
+    // POST
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    // PUT
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
     }
 
 }
